765399db5a19ebea380564555fd86b9be816fc99
Silvana Mora <silvanamorachaves@gmail.com>
2023-09-19 12:29:56 -0500

    portada

--- src/view.js
+++ src/view.js
@@ -4,3 +4,11 @@ export const renderItems = () => {
   return 'example';
 };
 
+
+document.querySelector("button").addEventListener("click", function() {
+  document.querySelector("#tituloPag2").style.opacity=  1;
+  document.querySelector("#titulo").style.opacity = 0;
+  document.querySelector("h2").style.opacity = 0;
+  document.querySelector("button").style.opacity = 0;
+  document.querySelector("#gifPaises").style.opacity = 0;
+});
